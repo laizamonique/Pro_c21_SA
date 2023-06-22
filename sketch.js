a65ba1dce06d8fe7c70030a3ad4bfb7995657b57
laizamonique <133267385+laizamonique@users.noreply.github.com>
2023-06-22 16:38:58 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,17 +9,32 @@ var ground;
 var left;
 var right;
 var top_wall;
+var ball;
+ var btn1;
+ var btn2;
 
 function setup() {
   createCanvas(400,400);
   engine = Engine.create();
   world = engine.world;
+  btn1 = createImg('right.png');
+  btn1.position(220,30);
+  btn1.size(50,50);
+  btn1.mouseClicked(hforce);
+  btn2 = createImg('up.png');
+  btn2.position(20,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vforce);
   
   ground =new Ground(200,390,400,20);
   right = new Ground(390,200,20,400);
   left = new Ground(10,200,20,400);
   top_wall = new Ground(200,10,400,20);
- 
+ var ball_options = {
+  restitution: 0.95 
+ }  
+  ball = Bodies.circle(200,100,20,ball_options);
+  World.add(world,ball);
   rectMode(CENTER);
   ellipseMode(RADIUS);
 }
@@ -27,10 +42,20 @@ function setup() {
 function draw() 
 {
   background(51);
+  
+  ellipse(ball.position.x,ball.position.y,20);
+  
   ground.show();
   top_wall.show();
   left.show();
   right.show();
   Engine.update(engine);
 }
-
+function hforce()
+{
+matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
+}  
+function vforce()
+{
+matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+}  
